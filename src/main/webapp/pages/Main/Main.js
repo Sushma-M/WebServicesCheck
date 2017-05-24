2a30aa7c214f4dd6fe8a87cd8e1748cd90a61fb3
Sairama Krishna Bonala <krishna.bonala@gmail.com>
2017-05-24 06:53:51 +0000

    Added Extra APIs New files added - [SOAP,googleapis_type,reimaginebanking_post,rechtspraak,randomuser2,yahooapis_noproxy,reimaginebanking_put,yahooapis,pdf2image,google_atom,fda_chart,reimaginebanking_delete,WM_live_multipart,FileService,web2pdf,WM_live_with_proxy,securityService,quandl,randomuser,WM_live_with_security,emailValidator,googleapis_json,iconfinder,googleapis] Files modified - [Main,leftnav,webapp,profiles,.wmproject.properties,pom.xml,project-services.xml,pages-config.json]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,17 +1,150 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
     };
 
+
+    $scope.GoogleapisInvokeonSuccess = function(variable, data) {
+        $scope.Widgets.accordionpane1.badgetype = "success";
+        $scope.Widgets.accordionpane1.badgevalue = "success";
+    };
+
+
+    $scope.Googleapis_jsonInvokeonSuccess = function(variable, data) {
+        $scope.Widgets.accordionpane2.badgetype = "success";
+        $scope.Widgets.accordionpane2.badgevalue = "success";
+    };
+
+
+    $scope.googleapis_typeInvokeonSuccess = function(variable, data) {
+        $scope.Widgets.accordionpane3.badgetype = "info";
+        if ($scope.Widgets.formatToggle1.datavalue == "xml") {
+            $scope.Widgets.accordionpane3.badgevalue = "XML";
+        } else if ($scope.Widgets.formatToggle1.datavalue == "json") {
+            $scope.Widgets.accordionpane3.badgevalue = "JSON";
+        }
+    };
+
+
+    $scope.Google_atomInvokeonSuccess = function(variable, data) {
+        $scope.Widgets.accordionpane4.badgetype = "success";
+        $scope.Widgets.accordionpane4.badgevalue = "success";
+    };
+
 }]);
+
+Application.$controller("GoogleServiceGrid1Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("GoogleServiceGrid2Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("GoogleServiceGrid3Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("grid4Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid5Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid7Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid8Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid8_1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid9Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid10Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid11Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid12Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid13Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid14Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid15Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
