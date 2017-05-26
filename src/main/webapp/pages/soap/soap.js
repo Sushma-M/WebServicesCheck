8187671b8b904b9b8613e9057bda6908b40c5385
Sairama Krishna Bonala <krishna.bonala@gmail.com>
2017-05-26 10:19:32 +0000

    Removed start updates New files added - [soap,reimaginebanking_get,securityService,hrdb,CountryInfoService,calc,data] Files modified - [Main,leftnav,profiles,.wmproject.properties,pom.xml,project-services.xml,app.variables.json,pages-config.json] Removed files - [SOAP]

--- src/main/webapp/pages/soap/soap.js
+++ src/main/webapp/pages/soap/soap.js
@@ -1,8 +1,8 @@
-Application.$controller("SOAPPageController", ["$scope", function ($scope) {
+Application.$controller("soapPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -14,4 +14,21 @@ Application.$controller("SOAPPageController", ["$scope", function ($scope) {
          */
     };
 
+
+
 }]);
+
+
+Application.$controller("grid1Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("grid2Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
